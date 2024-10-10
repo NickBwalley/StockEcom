441e7b5066697042a9196c64183f7511490b3bbe
Battlearmour2000 <vishani.mj@gmail.com>
2024-10-10 14:01:02 +0200

    added cors header update

--- StockEcomm/settings.py
+++ StockEcomm/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'products',
@@ -78,6 +79,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -106,6 +108,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'StockEcomm.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Allow requests from localhost (React dev server)
+    "",  # Replace with your actual frontend domain
+]
 
 
 # Database
